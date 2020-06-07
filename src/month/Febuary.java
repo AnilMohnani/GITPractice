51d5b62cf8513ef633fdb68f2423104ae4f0c62a
Anil <mohnani.anil@gmail.com>
2020-06-07 22:15:21 +0530

    Git Practise 1st Commit

---
+++ src/month/Febuary.java
@@ -0,0 +1,13 @@
+package month;
+
+public class Febuary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am Febuary");
+		System.out.println("I am Second Month");
+		System.out.println("I am having 28 days");
+	}
+
+}
---
+++ src/month/January.java
@@ -0,0 +1,10 @@
+package month;
+
+public class January {
+public static void main(String[] args) {
+	
+	System.out.println("I am January");
+	System.out.println("I am First Month");
+	System.out.println("I am having 31 days");
+}
+}
